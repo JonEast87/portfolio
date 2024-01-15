7d7b00637b8f4547de7b93960f7097b3fabb7c05
JonEast87 <j.d.east@icloud.com>
2024-01-15 13:19:58 -0700

    establishing structures and some basics, will begin with header work of the site

---
+++ lib/data.ts
@@ -0,0 +1,83 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import reservation from "@/public/reservation-system.png";
+import grubdash from "@/public/grubdash.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Thinkful",
+    location: "Miami, FL",
+    description: "Software Engineering Immersion camp.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2022",
+  },
+  {
+    title: "Arizona State University",
+    location: "Tempe, AZ",
+    description: "Backelors of Science in Graphic Information Technology",
+    icon: React.createElement(LuGraduationCap),
+    date: "2019 - 2024",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Reservation System",
+    description: "",
+    tags: ["React", "Node", "Express", "Knex", "PostgreSQL"],
+    imageUrl: reservation,
+  },
+  {
+    title: "GrubDash",
+    description: "",
+    tags: ["React", "Node", "Express", "Knex"],
+    imageUrl: grubdash,
+  },
+] as const;
+
+export const skillsData = [
+  "CSS",
+  "HTML",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "MongoDB",
+  "Express",
+  "PostgreSQL",
+  "Python",
+  "Django",
+  "Framer Motion",
+] as const;
