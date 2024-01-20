bacc6502dc1dfde1f844f7875e7c694547317cdb
JonEast87 <j.d.east@icloud.com>
2024-01-20 09:38:45 -0700

    finished the project and projects animating, and fixed the card defects as well

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,9 @@
-"use client";
+// removed use client here as I am seperating project and projects from each other
 
 import React from "react";
 import SectionHeading from "./section-heading";
-import Image from "next/image";
 import { projectsData } from "@/lib/data";
-
-import { motion } from "framer-motion";
+import Project from "./project";
 
 export default function Projects() {
   return (
@@ -19,38 +17,6 @@ export default function Projects() {
           </React.Fragment>
         ))}
       </div>
-    </section>
-  );
-}
-
-// Because the data object was already defined in data.ts using the typeof structure of the
-// objects will implicity declare the needed types
-type ProjectProps = (typeof projectsData)[number];
-
-function Project({ title, description, tags, imageUrl }: ProjectProps) {
-  return (
-    <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8">
-      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-        <h3 className="text-2x1 font-semibold">{title}</h3>
-        <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-        <ul className="flex flex-wrap mt-4 gap-2">
-          {tags.map((tag, index) => (
-            <li
-              className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
-              key={index}
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <Image
-        src={imageUrl}
-        alt="Jonathan portrait"
-        quality="95"
-        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2x1"
-      />
     </section>
   );
 }
