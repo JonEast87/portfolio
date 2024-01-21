2277462226d113ea761168c3c3d06abc93b24033
JonEast87 <j.d.east@icloud.com>
2024-01-21 09:25:50 -0700

    working on manual scroll update with the hooks

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,22 @@
 // removed use client here as I am seperating project and projects from each other
 
-import React from "react";
+import React, { useEffect } from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Projects() {
+  const { ref, inView } = useInView();
+  const { setActiveSection } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView) {
+      setActiveSection("About");
+    }
+  }, [inView, setActiveSection]);
+
   return (
     <section id="projects" className="scroll-mt-28">
       <SectionHeading>My projects</SectionHeading>
